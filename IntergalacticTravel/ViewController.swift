33e4fc91809afa50b90634f1f33206c4091c5e7f
Vlad Muradov <vmuradov9273@jh-mba158-11.d214.org>
2018-11-01 11:27:57 -0500

    Initial Commit

---
+++ IntergalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalacticTravel
+//
+//  Created by Vlad Muradov on 11/1/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
